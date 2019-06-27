973780a9d364f7a8b1102fc311fe16cd698ee0f0
ZRYao <yyy110011@livemail.tw>
2019-06-27 14:09:03 +0800

    20190627 update

--- formatTrans.py
+++ formatTrans.py
@@ -31,38 +31,57 @@ class formatTrans():
         i = 0
         NAME = np.array(os.listdir(self.data_addr))#data["ImageID"].unique()
         data = data.groupby('ImageID')
+        T1 = time.time()
         for pic_name in NAME:
             pic_ID = pic_name[:-4]
-            t1 = time.time()
+            #t1 = time.time()
             pic_addr = self.data_addr + '/' +  pic_name
             out_string += pic_addr + ' ' 
         # for each labeled box
             object_data = data.get_group(pic_ID)
-            t1_1 = time.time()
+            #t1_1 = time.time()
             object_data = np.array(object_data)
-            t2 = time.time()
-            #with Image.open(pic_addr) as pic:
-            #        height, width = pic.size
+            #t2 = time.time()
+            with Image.open(pic_addr) as pic:
+                    height, width = pic.size
             for j in range(len(object_data)):
-                x_min = '%.2f' % (object_data[j][4])
-                y_min = '%.2f' % (object_data[j][6])
-                x_max = '%.2f' % (object_data[j][5])
-                y_max = '%.2f' % (object_data[j][7])
+                x_min = int(object_data[j][4] * width)
+                y_min = int(object_data[j][6] * height)
+                x_max = int(object_data[j][5] * width)
+                y_max = int(object_data[j][7] * height)
+                if x_max - x_min <= 0:
+                    x_max = x_min + 1
+                if y_max - y_min <= 0:
+                    y_max = y_min + 1
+
                 class_id = tag2obj[object_data[j][2]]
-                box_string = x_min + ',' + y_min + ',' + x_max + ',' + y_max + ',' + str(class_id) + ' '
+                box_string = str(x_min) + ',' + str(y_min) + ',' + str(x_max) + ',' + str(y_max) + ',' + str(class_id) + ' '
                 
                 out_string += box_string
             out_string += '\n'
-            t3 = time.time()
+            output.write(out_string)
+            out_string = ''
+            #t3 = time.time()
             #print('after loc :'+ str(t1_1 - t1) + 'after np array:' + str(t2 - t1_1) + '  after box loop:' + str(t3 - t2))
 
-            if i % 100 ==0:
+            if i % 10000 ==0:
                 print(str(i) + ' of the file is finished')
             i += 1
-            #if i == 100:
+            #if i == 100000:
             #    break
-        output.write(out_string)
+        
+        T2 = time.time()
+        print("Time cose: " + str(T2 - T1))
         output.close()
+
+    def out_class(self):
+        out = ''
+        out_addr_ptr = open('./class.txt', 'w')
+        tag2obj = pd.read_csv(self.class_descriptions_addr, names=['tag', 'object'])
+        for i in tag2obj['object']:
+            out += i + '\n'
+        out_addr_ptr.write(out)
+
 
 
 if __name__ == "__main__":
@@ -71,4 +90,7 @@ if __name__ == "__main__":
                    'class-descriptions-boxable.csv',
                    #'/media/yyy/D4EC13B5EC13913A/DLProj/KaggleCompetition/transformed.txt')
                    'transformed.txt')
-    trans.run()
+    if 1:
+        trans.run()
+    if 0:
+        trans.out_class()
